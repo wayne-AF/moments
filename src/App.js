e349ae75d4c4a60fd5dfab27fdad6fd1798f3be8
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-03-20 16:15:15 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }

95c1143bd0fa773c2294f780db10b802b7de7a2b
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-03-22 19:15:45 +0000

    added signin and signup forms

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,159 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import axios from "axios";
+
+const SignUpForm = () => {
+  // Using hooks for signup data
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  // Allows us to call push method with where we want to go in
+  // the handleSubmit
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      // Spread in previous signup data so we only need to
+      // update relevant attribute.
+      // This creates a key:value pair, with the key being the input field
+      // name, and the value being the value entered by the user. This means
+      // we can reuse this function to handle changes to any of our input 
+      // fields instead of writing separate handlers for each one.
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // Formsubmit handler
+  const handleSubmit = async (event) => {
+    // Stops the page refreshing
+    event.preventDefault();
+    // Sends all the signupdata to the endpoint in our API
+    // application for user registration.
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      // Uses the useHistory hook set above
+      history.push("/signin");
+      // Storing the errors in the state so we can display to user when
+      // something goes wrong. 
+    } catch (err) {
+      // ? = optional chaining: checks if response is defined before looking
+      // for data. If response isn't defined, it won't throw an error.
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign up</h1>
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="choose a username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {/* Displaying error to users */}
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="choose a password"
+                name="password1"
+                value={password1}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password1?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="confirm your password"
+                name="password2"
+                value={password2}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password2?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign up
+            </Button>
+            {/* Alert for when passwords don't match */}
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signin">
+            Already have an account? <span>Sign in</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
